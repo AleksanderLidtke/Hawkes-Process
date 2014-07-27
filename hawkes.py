e8bfb41ad739a18e4c43f008ce6987fe3a421c2e
AleksanderLidtke_HP <alek_l@onet.eu>
2014-07-27 23:06:58 +0100

    Started working with real epochs
    Rather than using artificial epochs of events (trades) started
    converting to using true epochs. This will enable actual inter-arrival
    times to be computed and hence the distribution of those with the
    rescaled times to be analysed.

--- hawkes.py
+++ hawkes.py
@@ -33,12 +33,14 @@ all_counts = pandas.DataFrame({'counts': np.ones(len(all_trades))}, index=all_tr
 empirical_1min = all_counts.resample('1min', how='sum') # Bin the data so find the number of trades in 1 minute intervals.
 #ax = empirical_1min.fillna(method='pad').plot() # Pandas plotting.
 
-baseEventTimes = np.array( range(len(empirical_1min.values)), dtype=np.float64) # Dummy times when the events take place, don't care too much about actual epochs where the bins are placed.
+baseEventTimes = all_counts.index.astype(np.int64)/ 10**9 # Times when the events take place as seconds from epoch.
+baseEventTimes = baseEventTimes.astype(np.float64) # Convert to floats to avoid errors in division etc.
+
 eventTimes = [] # With the event batches split into separate events.
 for i in range(len(empirical_1min.values)): # Deal with many events occurring at the same time - need to distinguish between them by splitting each batch of events into distinct events taking place at almost the same time.
     if not np.isnan(empirical_1min.values[i]):
         for j in range(empirical_1min.values[i]):
-            eventTimes.append(baseEventTimes[i]+0.000001*(j+1)) # For every event that occurrs at this epoch enter a dummy event very close to it in time that will increase the conditional intensity.
+            eventTimes.append( empirical_1min.index.astype(np.int64)[i] + 0.000001*(j+1)) # For every event that occurrs at this epoch enter a dummy event very close to it in time that will increase the conditional intensity.
 
 eventTimes = np.array( eventTimes, dtype=np.float64 ) # Change to array for ease of operations.
 
@@ -49,25 +51,27 @@ mu = 0.07 # Parameter values as found in the article.
 alpha = 1.18
 beta = 1.79
 
+#TODO: scale these times to true epochs.
 " Compute conditional intensities for all epochs using the Hawkes process - add more points to see how the effect of individual events decays over time. "
 conditionalIntensitiesPlotting = [] # Conditional intensity for every epoch of interest.
 timesOfInterest = np.linspace(eventTimes.min(), eventTimes.max(), eventTimes.size*10) # Times where the intensity will be sampled. Sample at much higher frequency than the events occur at.
 for t in timesOfInterest:
     conditionalIntensitiesPlotting.append( mu + np.array( [alpha*math.exp(-beta*(t-ti)) if t > ti else 0.0 for ti in eventTimes] ).sum() ) # Find the contributions of all preceding events to the overall chance of another one occurring. All events that occur after time of interest t have no contribution.
 
+#TODO: scale these times to true epochs.
 " Compute conditional intensities at the same epochs as the empirical data are known. "
 conditionalIntensities=[] # This will be used in the QQ plot later, has to have the same size as the empirical data.
 for t in np.linspace(eventTimes.min(), eventTimes.max(), eventTimes.size):
     conditionalIntensities.append( mu + np.array( [alpha*math.exp(-beta*(t-ti)) if t > ti else 0.0 for ti in eventTimes] ).sum() ) # Use eventTimes here as well to feel the influence of all the events that happen at the same time.
 
 " Compute integrals of the conditional intensity from t=0 to current time t_i. "
-integralsOfConditionalIntensity = np.zeros( len(eventTimes) )
-for i in range( eventTimes.size): # Find integral of the conditional intensity at eery time, index i.
+integralsOfConditionalIntensity = np.zeros( baseEventTimes.size )
+for i in range( baseEventTimes.size ): # Find integral of the conditional intensity at eery time, index i.
     for j in range(i): # Sum all the conditional intensities from the beginning of the analysis interval up to the current epoch i.
         integralsOfConditionalIntensity[i] = integralsOfConditionalIntensity[i] + conditionalIntensities[j]
 
-interArrivalTimes = np.zeros( eventTimes.size ) # Times between consecutive events.
-interArrivalTimes[1:] = eventTimes[1:] - eventTimes[:-1] # First event is when we start counting time, no interarrival time for it.
+interArrivalTimes = np.zeros( baseEventTimes.size ) # Times between consecutive events.
+interArrivalTimes[1:] = baseEventTimes[1:] - baseEventTimes[:-1] # First event is when we start counting time, no interarrival time for it.
 
 """ PLOT THE EMPIRICAL DATA AND FITTED CONDITIONAL INTENSITIES. """
 fig = matplotlib.pyplot.figure()
@@ -78,21 +82,21 @@ ticksFontSize = 14
 
 fig.suptitle(r"$Conditional\ intensity\ VS\ time$", fontsize=20)
 ax.grid(True)
-ax.set_xlabel(r'$Time$',fontsize=labelsFontSize)
+ax.set_xlabel(r'$Time\ (seconds\ since\ epoch)$',fontsize=labelsFontSize)
 ax.set_ylabel(r'$\lambda$',fontsize=labelsFontSize)
 matplotlib.rc('xtick', labelsize=ticksFontSize) 
 matplotlib.rc('ytick', labelsize=ticksFontSize)
 
 # Plot the empirical binned data.
-ax.plot(baseEventTimes,empirical_1min.values, color='blue', linestyle='solid', marker=None, markerfacecolor='blue', markersize=12)
+ax.plot(empirical_1min.index.to_pydatetime(), empirical_1min.values, color='blue', linestyle='solid', marker=None, markerfacecolor='blue', markersize=12)
 empiricalPlot = matplotlib.lines.Line2D([],[],color='blue', linestyle='solid', marker=None, markerfacecolor='blue', markersize=12)
 
 # And the fit obtained using the Hawkes function.
-ax.plot(timesOfInterest, conditionalIntensitiesPlotting, color='red', linestyle='solid', marker=None, markerfacecolor='blue', markersize=12)
-fittedPlot = matplotlib.lines.Line2D([],[],color='red', linestyle='solid', marker=None, markerfacecolor='blue', markersize=12)
+ax.plot(timesOfInterest, conditionalIntensitiesPlotting, color='red', linestyle='solid', marker=None, markerfacecolor='red', markersize=12)
+fittedPlot = matplotlib.lines.Line2D([],[],color='red', linestyle='solid', marker=None, markerfacecolor='red', markersize=12)
      
 fig.legend([fittedPlot, empiricalPlot], [r'$Fitted\ data$', r'$Empirical\ data$'])
-matplotlib.pyplot.show()
+fig.show()
 
 """ PLOT THE DISTRIBUTION OF THE INTERARRIVAL TIMES IN THE RESCALED TIME. """
 fig2 = matplotlib.pyplot.figure()
@@ -107,7 +111,81 @@ matplotlib.rc('ytick', labelsize=ticksFontSize)
 distributionScatter = ax2.scatter(integralsOfConditionalIntensity, interArrivalTimes, c='blue', marker='x') # If these are close to the straight line with slope line these points come from a normal distribution.
    
 fig2.legend([distributionScatter], [r'$Actual\ distribution$'])
+fig2.show()
+
+""" MAKE A QUANTILE PLOT OF THE INTERARRIVAL TIMES. """
+" Process the data and compute the quantiles. "
+orderStatistics = integralsOfConditionalIntensity;
+orderStatistics2 = interArrivalTimes;
+
+orderStatistics.sort(axis=0) # Need to sort data in ascending order to make a QQ plot. orderStatistics is a column vector.
+orderStatistics2.sort()
+
+smapleQuantiles=np.zeros( orderStatistics.size ) # Quantiles of the integralsOfConditionalIntensity i.e. the rescaled time.
+smapleQuantiles2=np.zeros( orderStatistics2.size ) # Quantiles of the interArrivalTimes.
+for i in range( orderStatistics.size ):
+    temp = int( 100*(i-0.5)/float(smapleQuantiles.size) ) # (i-0.5)/float(smapleQuantiles.size) th quantile. COnvert to % as expected by the numpy function.
+    if temp<0.0:
+        temp=0.0 # Avoid having -ve percentiles.
+    smapleQuantiles[i] = np.percentile(orderStatistics, temp)
+    smapleQuantiles2[i] = np.percentile(orderStatistics2, temp)
+
+" Make a quantile plot of inter-arrival times. "
+fig3 = matplotlib.pyplot.figure()
+ax3 = fig3.gca(aspect="equal")
+
+fig3.suptitle(r"$Quantile\ plot\ of\ inter-arrival\ times$", fontsize=20)
+ax3.grid(True)
+ax3.set_xlabel(r'$Sample\ fraction\ (\%)$',fontsize=labelsFontSize)
+ax3.set_ylabel(r'$Ordered\ values$',fontsize=labelsFontSize)
+matplotlib.rc('xtick', labelsize=ticksFontSize) 
+matplotlib.rc('ytick', labelsize=ticksFontSize)
+
+distScatter = ax3.scatter(smapleQuantiles2, orderStatistics2, c='blue', marker='o') # If these are close to the straight line with slope line these points come from a normal distribution.
+
+ax3.plot(smapleQuantiles2, smapleQuantiles2, color='red', linestyle='solid', marker=None, markerfacecolor='red', markersize=12)
+normalDistPlot = matplotlib.lines.Line2D([],[],color='red', linestyle='solid', marker=None, markerfacecolor='red', markersize=12)
+     
+fig3.legend([normalDistPlot, distScatter], [r'$Normal\ distribution$', r'$Inter-arrival\ times$'])
+fig3.show()
+
+" Make a quantile plot of rescaled times. "
+fig4 = matplotlib.pyplot.figure()
+ax4 = fig4.gca(aspect="equal")
+
+fig4.suptitle(r"$Quantile\ plot\ of\ rescaled\ times$", fontsize=20)
+ax4.grid(True)
+ax4.set_xlabel(r'$Sample\ fraction\ (\%)$',fontsize=labelsFontSize)
+ax4.set_ylabel(r'$Ordered\ values$',fontsize=labelsFontSize)
+matplotlib.rc('xtick', labelsize=ticksFontSize) 
+matplotlib.rc('ytick', labelsize=ticksFontSize)
+
+distScatter = ax4.scatter(smapleQuantiles, orderStatistics, c='blue', marker='o') # If these are close to the straight line with slope line these points come from a normal distribution.
+
+ax4.plot(smapleQuantiles, smapleQuantiles, color='red', linestyle='solid', marker=None, markerfacecolor='red', markersize=12)
+normalDistPlot = matplotlib.lines.Line2D([],[],color='red', linestyle='solid', marker=None, markerfacecolor='red', markersize=12)
+     
+fig4.legend([normalDistPlot, distScatter], [r'$Normal\ distribution$', r'$Rescaled\ times$'])
 matplotlib.pyplot.show()
+
+" QQ plot of inter-arrival times and the rescaled times. "
+fig5 = matplotlib.pyplot.figure()
+ax5 = fig5.gca(aspect="equal")
+
+fig5.suptitle(r"$Quantile\ -\ Quantile\ plot$", fontsize=20)
+ax5.grid(True)
+ax5.set_xlabel(r'$Quantiles\ of\ the\ rescaled\ time$',fontsize=labelsFontSize)
+ax5.set_ylabel(r'$Quantiles\ of\ the\ inter-arrival\ times$',fontsize=labelsFontSize)
+matplotlib.rc('xtick', labelsize=ticksFontSize) 
+matplotlib.rc('ytick', labelsize=ticksFontSize)
+
+distributionScatter = ax5.scatter(smapleQuantiles, smapleQuantiles2, c='blue', marker='x') # If these are close to the straight line with slope line these points come from a normal distribution.
+
+ax5.plot(smapleQuantiles, smapleQuantiles, color='red', linestyle='solid', marker=None, markerfacecolor='red', markersize=12)
+normalDistPlot2 = matplotlib.lines.Line2D([],[],color='red', linestyle='solid', marker=None, markerfacecolor='red', markersize=12)
+     
+fig5.legend([normalDistPlot2, distributionScatter], [r'$Normal\ distribution$', r'$Comparison\ of\ datasets$'])
+fig5.show()
 
 # =============================================================================
 # No longer needed, only kept in case it ever becomes useful.
